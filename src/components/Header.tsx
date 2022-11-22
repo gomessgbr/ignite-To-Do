989a1430c22a4d543f66fed3dffcab3948db5952
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-11-22 09:46:32 -0300

    feat: insert new logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,11 @@
 import styles from "./Header.module.css";
 
-import rocket from "../assets/rocket.svg";
 import todoLogo from "../assets/todoLogo.svg";
 
 export function Header() {
   return (
     <header className={styles.container}>
-      <div className={styles.imageContainer}>
-        <img src={rocket} alt="Logo foguete" />
-        <img src={todoLogo} alt="Logo todo" />
-      </div>
+      <img src={todoLogo} alt="Logo todo" />
     </header>
   );
 }
