500030748a3654eb4fca6b2f845efa4895367ac3
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-11-22 11:22:10 -0300

    feat: icons PlusCircle

--- src/components/InputTask.tsx
+++ src/components/InputTask.tsx
@@ -1,16 +1,22 @@
+import { PlusCircle } from "phosphor-react";
 import styles from "./InputTask.module.css";
 
 export function InputTask() {
   return (
     <div className={styles.container}>
-      <form>
-        <input
-          type="text"
-          placeholder="Adicione uma nova tarefa"
-          className={styles.Input}
-        />
-        <button type="submit">Criar +</button>
-      </form>
+      <header className={styles.inputTaskContainer}>
+        <form>
+          <input
+            type="text"
+            placeholder="Adicione uma nova tarefa"
+            className={styles.inputTask}
+          />
+          <button type="submit" className={styles.inputButton}>
+            Criar
+            <PlusCircle size={20} />
+          </button>
+        </form>
+      </header>
     </div>
   );
 }
