77b627767582e2349676f34df569bded83fe05c9
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-11-23 09:14:41 -0300

    feat:

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,5 +1,16 @@
+import { Trash } from "phosphor-react";
 import styles from "./Tasks.module.css";
 
 export function Tasks() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div>
+        <p>Tarefas Criadas</p>
+        <p>Concluídas</p>
+      </div>
+      <div>
+        <Trash size={24} />
+      </div>
+    </div>
+  );
 }
