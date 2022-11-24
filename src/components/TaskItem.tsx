384bf80faffe712cd594ad6e1c5d0bdbb35d8c14
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-11-24 05:52:03 -0300

    feat: taskItem style and content

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -3,5 +3,16 @@ import { Trash } from "phosphor-react";
 import styles from "./TaskItem.module.css";
 
 export function TaskItem() {
-  return <div className={styles.containerTaskItem}></div>;
+  return (
+    <div className={styles.containerTaskItem}>
+      <button className={styles.radioButton} />
+      <div className={styles.contentTask}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque quae
+        hic tempore placeat, atque beatae.
+      </div>
+      <button className={styles.deleteButton}>
+        <Trash size={20} />
+      </button>
+    </div>
+  );
 }
