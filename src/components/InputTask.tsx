524c5875cf32be574e362275c55fbeb0f6798041
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-11-26 17:00:35 -0300

    feat: change InputTask component

--- src/components/InputTask.tsx
+++ src/components/InputTask.tsx
@@ -1,11 +1,19 @@
 import { PlusCircle } from "phosphor-react";
+import { FormEvent } from "react";
 import styles from "./InputTask.module.css";
 
-export function InputTask() {
+interface InputTaskProps {
+  createNewTask: (event: FormEvent) => void;
+}
+
+export function InputTask({ createNewTask }: InputTaskProps) {
+  function handleNewTask(event: FormEvent) {
+    createNewTask(event);
+  }
   return (
     <div className={styles.containerInputTask}>
       <header className={styles.inputTaskContainer}>
-        <form>
+        <form onSubmit={handleNewTask}>
           <input
             type="text"
             placeholder="Adicione uma nova tarefa"
